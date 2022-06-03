d683dac0956a160dd1358672629d0b0dc32598da
caesarhu <hupeishun@gmail.com>
2022-06-04 05:51:50 +0800

    add ispalindrome.

--- src/MathUtils.jl
+++ src/MathUtils.jl
@@ -2,11 +2,27 @@ module MathUtils
 
 export tonumber
 """
-digits collect to number
+digits collect to number.
 tonumber(d)
 """
 function tonumber(d; base=10)
     sum(d.*base .^(0:(length(d)-1)))
+end
+
+export ispalindrome
+"""
+check palindromic number.
+ispalindrome(v)
+"""
+function ispalindrome(s)
+    len = length(s)
+    if(len==0 || len==1)
+        return true
+    end
+    if(s[1] == s[len])
+        return ispalindrome(s[2:len-1])
+    end
+    return false
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,3 +3,7 @@ using MathUtils
 
 @test tonumber(digits(10)) == 10
 @test tonumber(digits(10, base=2), base=2) == 10
+@test ispalindrome(digits(12321))
+@test ispalindrome("12321")
+@test ispalindrome(digits(12320)) == false
+@test ispalindrome("12320") == false
